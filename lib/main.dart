f6270043e5ba8b207c8e2ea747057bb1779951a1
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-21 02:06:33 +0545

    3 ways to add images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// #8 - Images & Assets
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
@@ -6,24 +8,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "My First APP",
-          style: TextStyle(),
+        appBar: AppBar(
+          title: const Text(
+            "My First APP",
+            style: TextStyle(),
+          ),
+          backgroundColor: Colors.red,
+          centerTitle: true,
         ),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          "hello world!!!",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              fontFamily: 'IndieFlower'),
-        ),
-      ),
-    );
+        body: Center(
+          // three ways to add image:
+
+          // child: Image(image: AssetImage('assets/sunset.jpg')),
+          // child: Image.asset('assets/sunflower.jpg'),
+          child: Image.network(
+              'https://plus.unsplash.com/premium_photo-1703606029674-a461a6ee8045?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+        ));
   }
 }
