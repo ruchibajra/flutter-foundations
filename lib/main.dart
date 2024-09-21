31185ebf6dde7a70d4037a97189e8a1baaaffe55
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-22 00:23:18 +0545

    custom class and named parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // #8 - Images & Assets
 
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(QuoteList());
 
@@ -12,10 +13,17 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    "Be yourself! everyone else is already taken.",
-    "It's okay to say no to things that you WANT and focus on things that you NEED to do",
-    "Its better to go slowly than not to move at all"
+  List<Quote> quotes = [
+    Quote(
+        author: 'Ellon Mosk',
+        text: 'Be yourself! everyone else is already taken.'),
+    Quote(
+        author: 'Mark Zuckerberg',
+        text:
+            'Its okay to say no to things that you WANT and focus on things that you NEED to do'),
+    Quote(
+        author: 'Yrary',
+        text: 'Its better to go slowly than not to move at all')
   ];
 
   @override
@@ -30,7 +38,9 @@ class _QuoteListState extends State<QuoteList> {
         foregroundColor: Colors.white,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
 
         // Same thing as above, as arrow fn can be wriiten in one line so we used above code.
         // children: quotes.map((quote) {
