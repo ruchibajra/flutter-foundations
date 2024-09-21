171fadb02d270f4a3256def32ca8017594e1a691
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-21 23:27:05 +0545

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(NinjaCard());
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjalevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +23,16 @@ class NinjaCard extends StatelessWidget {
           backgroundColor: Colors.grey[850],
           foregroundColor: Colors.white,
           elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjalevel += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+          foregroundColor: Colors.white,
         ),
         body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -48,7 +65,7 @@ class NinjaCard extends StatelessWidget {
                   style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
               SizedBox(height: 10.0),
               Text(
-                '8',
+                '$ninjalevel',
                 style: TextStyle(
                     color: Colors.amberAccent[200],
                     letterSpacing: 2.0,
@@ -79,5 +96,19 @@ class NinjaCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
