a8bedd1ea9d434f6ccdd2b162b0c2d6aab50d05d
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-21 23:11:31 +0545

    ninja id project

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,82 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(NinjaCard());
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: const Text(
-            "My First APP",
-            style: TextStyle(),
-          ),
-          backgroundColor: Colors.blue,
+          title: Text('Ninja ID Card'),
           centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          foregroundColor: Colors.white,
+          elevation: 0.0,
         ),
-        body: Row(
-          children: [
-            Expanded(flex: 5, child: Image.asset('assets/sunset.jpg')),
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                color: Colors.red,
-                child: Text('1'),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/ninja.png'),
+                  radius: 40.0,
+                ),
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                color: Colors.blue,
-                child: Text('2'),
+              Divider(
+                height: 90.0,
+                color: Colors.grey[800],
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                color: Colors.green,
-                child: Text('3'),
+              Text('NAME',
+                  style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+              SizedBox(height: 10.0),
+              Text(
+                'Ruchi',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
               ),
-            )
-          ],
-        ));
+              SizedBox(height: 30.0),
+              Text('CURRENT NINJA LEVEL',
+                  style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+              SizedBox(height: 10.0),
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'ruchibajra@gmail.com',
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-// NOTE: Container Widget can have padding, margin, colors and so on but if you want
-//  to have only padding and no other properties then can use the widget Padding.
