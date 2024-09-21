0db03bb913e974eb8b7b40fbd098d818be37ac2c
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-21 16:00:26 +0545

    column

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,30 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text("hello world"),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.brown),
-              onPressed: () {},
-              child: Text('click me'),
+            Row(
+              children: [
+                Text("text 1 from the row"),
+                Text("text 2 from the row"),
+              ],
             ),
             Container(
+              child: Text("box 1"),
+              padding: EdgeInsets.all(30),
               color: Colors.red,
-              padding: EdgeInsets.all(50.0),
-              child: Text("inside the container"),
+            ),
+            Container(
+              child: Text("box 2"),
+              padding: EdgeInsets.all(50),
+              color: Colors.blue,
+            ),
+            Container(
+              child: Text("box 3"),
+              padding: EdgeInsets.all(80),
+              color: Colors.green,
             )
           ],
         ));
