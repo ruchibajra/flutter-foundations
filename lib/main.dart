72872ba693f6221d7afd1721041d763a25cbb2b4
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-22 20:00:42 +0545

    CARD and MAP

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,37 @@ class _QuoteListState extends State<QuoteList> {
         text: 'Its better to go slowly than not to move at all')
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,9 +69,7 @@ class _QuoteListState extends State<QuoteList> {
         foregroundColor: Colors.white,
       ),
       body: Column(
-        children: quotes
-            .map((quote) => Text('${quote.text} - ${quote.author}'))
-            .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
 
         // Same thing as above, as arrow fn can be wriiten in one line so we used above code.
         // children: quotes.map((quote) {
